a3f66be121734587c94ed84940c45f32da775cda
Benjamin Small <ben20262@aim.com>
2019-11-16 00:10:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,7 +35,7 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-  puts "Hi"
+  puts cart
   puts coupons
   coupons.each do |ticket|
     counter = 0
